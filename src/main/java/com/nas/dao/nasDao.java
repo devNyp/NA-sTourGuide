1b6056dc0cb376b83506343016bc3875f7cba75a
nyp <git.nasunsaeng@gmail.com>
2022-01-27 15:29:29 +0900

    first commit

---
+++ src/main/java/com/nas/dao/nasDao.java
@@ -0,0 +1,12 @@
+package com.nas.dao;
+
+import java.util.List;
+
+public interface nasDao {
+	public <T> void insert(T t);
+	public <T> T selectOn(String id);
+	public <T, K> List<T> selectAll(K k);
+	public <T> void update(T t);
+	public void delete(String id);
+	
+}
